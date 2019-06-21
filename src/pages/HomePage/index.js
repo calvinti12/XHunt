79f9bece873c8d8049743a780a0930443190426b
sharjeel imtiaz <imtiazsharjeel@gmail.com>
2019-06-21 16:37:42 +0500

    Basic structure & fonts included

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.scss';
+class HomePage extends React.Component {
+  constructor(props) {
+    super();
+  }
+  render() {
+    return (
+      <div className='container'>
+        <div className='homePage'>
+          <div className='header'>
+          </div>
+          <div className='content'>
+
+          </div>
+          <div className='footer'>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default HomePage;
